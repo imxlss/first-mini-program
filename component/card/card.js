5fa5680e3bac7ac6f7ae9fefd48db22213f23ce8
imxlss <imxlss@163.com>
2019-08-15 17:57:35 +0800

    feat:card 作为city维度上的组件

--- component/card/card.js
+++ component/card/card.js
@@ -1,22 +1,40 @@
+const globalData = getApp().globalData;
+
 Component({
   data: {
-    WEEKDAYS: ['日', '一', '二', '三', '四', '五', '六'],
-    CURRENTDAYS: ['今天', '明天', '后天'],
-    dayIndex: Number,
-    weekIndex: Number
+    iconUrl: globalData.request_url.icon,
+    weatherInfo: {}
   },
   properties: {
     // 这里定义了innerText属性，属性值可以在组件使用时指定
-    forecast: Object
+    cityName: {
+      value: '北京',
+      type: String
+    }
   },
   lifetimes: {
     // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
     attached: function () {
-      this.setData({
-        weekIndex: new Date(this.properties.forecast.date).getDay(),
-        dayIndex: new Date(this.properties.forecast.date).getDay() - new Date().getDay()
-      })
+      this.getNow(this.properties.cityName);
     },
     detached: function () {},
+  },
+  methods: {
+    getNow: function (location) {
+      wx.request({
+        url: globalData.request_url.now,
+        data: {
+          location,
+          key: globalData.key
+        },
+        success: res => {
+          if (res.statusCode === 200) {
+            this.setData({
+              weatherInfo: res.data.HeWeather6[0].now
+            });
+          }
+        }
+      })
+    }
   },
 });
